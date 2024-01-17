2844d505f1913fbbd0f3be2e5fe65d31c464f19f
Limson91 <matteo.a.colautti@gmail.com>
2024-01-17 11:27:20 +0100

    add - views blade.php

---
+++ resources/views/admin/projects/edit.blade.php
@@ -0,0 +1,58 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Create Project</h3>
+
+                <form action="{{route('admin.projects.store')}}" method="POST">
+
+                @csrf
+
+                <div class="form-group">
+                    <label for="title">Title:</label>
+                    <input type="text" name="title" id="title" class="form-control" value="{{old('title')}}">
+                </div>
+
+                <div class="form-group">
+                    <label for="image_path">Image:</label>
+                    <input type="text" name="image_path" id="image_path" class="form-control" value="{{old('image_path')}}">
+                </div>
+
+                <div class="form-group">
+                    <label for="description">Description:</label>
+                    <textarea name="description" id="description" class="form-control">{{old('description')}}</textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="type_id">Tags:</label>
+                    <select name="type_id" id="type_id" class="form-control">
+                        <option>Select Tags:</option>
+
+                        @foreach ($types as $type)
+                            <option value="{{$type->id}}" {{$project->type_id == $type->id ? 'selected' : ''}}>
+                                {{$type->name}}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="technologies">Tech:</label>
+                    <select name="technologies[]" id="technologies" class="form-control" multiple>
+                        @foreach ($technologies as $technology)
+                            <option value="{{$technology->id}}" {{in_array($technology->id, old('technologies', $project->technologies->pluck('id')->toArray())) ? 'selected' : ''}}>{{$technology->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Create Project</button>
+
+            </form>
+
+            <a href="{{route('admin.projects.index')}}" class="btn btn-secondary mt-2">Back to the List!</a>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,14 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>{{$project->title}}</h3>
+                <p>{{$project->description}}</p>
+                <img src="{{$project->image_path}}" alt="{{$project->title}}" class="img-fluid">
+                <a href="{{route('admin.projects.index')}}" class="btn btn-primary">Back to The List</a>
+            </div>
+        </div>
+    </div>
+@endsection
